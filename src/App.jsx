28cd74a02bf5f6af8df0faa4f63fda696a2a0b9d
shiwenjiao <slyviashi@didiglobal.com>
2024-05-28 11:44:55 +0800

    feat:practice the 1.5 受控表单

--- src/App.jsx
+++ src/App.jsx
@@ -3,54 +3,28 @@ import React, { useState } from "react";
 // import City from "./components/city";
 // import NameCall from "./components/nameCall";
 // import { LetterOne, Two, Three, Four } from "./letter/one";
-import Transform from "./letter/rmb_dollar转化";
+// import Transform from "./letter/rmb_dollar转化";
+import Checkboxpractice from "./components/checkboxpractice";
 
 function App() {
-  //   const somewhere = [
-  //     { id: 1, city: "Kyiv" },
-  //     { id: 2, city: "Warsaw" },
-  //     { id: 3, city: "湖州" },
-  //   ];
-  //   const nackName = ["小白", "比丢", "旺仔"];
-  //   const shuju = {
-  //     name: "小白",
-  //     age: 18,
-  //     gender: "other",
-  //   };
+  // const somewhere = [
+  //   { id: 1, city: "Kyiv" },
+  //   { id: 2, city: "Warsaw" },
+  //   { id: 3, city: "湖州" },
+  // ];
+  // const nackName = ["小白", "比丢", "旺仔"];
+  // const shuju = {
+  //   name: "小白",
+  //   age: 18,
+  //   gender: "other",
+  // };
   // const handleChange = (number) => {
   //   console.log("this number is", number);
   // };
-
-  const money = {
-    rmb: "",
-    dollar: "",
-  };
-  const [moneystate, setMoneystate] = useState(money);
-  //这里的value是输入框的值,
-  const transformToRMB = (value) => {
-    if (parseFloat(value) || value === "" || parseFloat(value) === 0) {
-      setMoneystate({
-        dollar: value,
-        rmb: value === "" ? "" : (parseFloat(value) * 6.5).toString(),
-      });
-    } else {
-      alert("请输入数字");
-    }
-  };
-  const transformToDollar = (value) => {
-    if (parseFloat(value) || value === "" || parseFloat(value) === 0) {
-      setMoneystate({
-        rmb: value,
-        dollar: value === "" ? "" : (parseFloat(value) / 6.5).toString(),
-      });
-    } else {
-      alert("请输入数字");
-    }
-  };
-
   return (
     <div className="App">
-      {/* <img
+      {/* <Transform />
+      <img
         src="https://kpi.ua/files/logo.png"
         alt="badges"
         className="badges"
@@ -63,20 +37,11 @@ function App() {
       <Three>
         <div>111</div>
         <div>插槽2</div>
-      </Three> */}
-      {/* <Four shuju={shuju} handleChange={handleChange} /> */}
-
-      <Transform
-        text={"Dollar"}
-        money={moneystate.dollar}
-        transform={transformToRMB}
-      />
-      <Transform
-        text={"RMB"}
-        money={moneystate.rmb}
-        transform={transformToDollar}
-      />
+      </Three>{" "}
+      <Four shuju={shuju} handleChange={handleChange} /> */}
+      <Checkboxpractice />
     </div>
   );
 }
+
 export default App;
--- src/letter/rmb_dollar转化.jsx
+++ src/letter/rmb_dollar转化.jsx
@@ -18,5 +18,7 @@ function Transform(props) {
     </div>
   );
 }
+//我发现子组件就是写一个通用的壳子,真正的执行逻辑还是写到了父组件中.
+//子组件:长什么样子,有什么属性;父组件进行具体的逻辑填充.
 
 export default Transform;
